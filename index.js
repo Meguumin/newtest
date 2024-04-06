95f5476b91b47db5adfd2745b4b28c76c606cd5c
Aiden Tong <aidenplaysosuandgames@gmail.com>
2024-04-06 12:16:55 -0700

    Basic Template

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();  
+
+
+
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname,'public','index.html'));
+});
+
+
+app.use(express.static(__dirname + "/public"));
+app.listen(3000, () => {
+  console.log('server started at 3000');
+});"";
+
+
+ 
